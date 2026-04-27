8418c7aed75ecb2d12cda6d59679b7d9686b4d56
Anuj21d <anujwork2410@gmail.com>
2026-04-27 10:22:28 +0530

    .

---
+++ JavaScript/Java Script Variable/script.js
@@ -0,0 +1,18 @@
+console.log("This Variable of Java Script");
+
+var a = 8;
+var b = 9;
+var c = "Anuj";
+
+console.log(typeof a + " " + typeof b + " " + typeof c);
+
+// object data type
+
+let o = {
+  name: "Anuj",
+  "company salary": 200000,
+};
+console.log(o);
+
+o.role= "developer";
+console.log(o);
