3a00da448dd341c2830e7fad9e261dbeb86f7037
Anuj21d <anujwork2410@gmail.com>
2026-04-29 16:43:25 +0530

    start with dom

---
+++ JavaScript/DOM/script.js
@@ -0,0 +1,10 @@
+console.log("Start Learning DOM")
+
+// document.body.firstElementChild
+// <div class=​"container">​…​</div>​
+// document.body.firstElementChild.nextElementSibling
+// <table>​…​</table>​
+// document.body.firstElementChild.childNodes
+// NodeList(9) [text, div.box, text, div.box, text, div.box, text, div.box, text]
+// document.body.firstElementChild.children
+// HTMLCollection(4) [div.box, div.box, div.box, div.box]
