7dda9a96580ba53da0b2c92abd687ea9ac800899
Anuj21d <anujwork2410@gmail.com>
2026-05-14 11:46:53 +0530

    start new project -CapMeSketch - AI Sketch Caption Generator and done navbar

---
+++ Projects/Html , Css And JavaScript/AI Sketch Caption Gentrator/script.js
@@ -0,0 +1,10 @@
+let links = document.querySelectorAll(".nav-link");
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    links.forEach((link) => {
+      link.classList.remove("active");
+    });
+    link.classList.add("active");
+  });
+});
