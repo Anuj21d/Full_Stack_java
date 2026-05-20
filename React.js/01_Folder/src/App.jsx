05e8cd69240a82e746a6c5468b8c26ddd9dd8268
Anuj21d <anujwork2410@gmail.com>
2026-05-20 18:40:49 +0530

    basic knowledge about folder structure and others

--- React.js/01_Folder/src/App.jsx
+++ React.js/01_Folder/src/App.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const App = () => {
   return (
-    <h1>
-      Hello I am Anuj
-    </h1>
+    <div className='container'>
+      <h2>Hello My self Anuj</h2>
+      <h3>I am A Frontend Developer</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi corporis voluptas aliquid, in natus iste. Officia deleniti praesentium, dolore molestias delectus officiis tenetur exercitationem dicta dolorem reiciendis, veritatis tempore ullam!</p>
+    </div>
   )
 }
 
